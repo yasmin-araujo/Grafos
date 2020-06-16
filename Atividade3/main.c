e723dd16236bd3efca25aeaebdf7473e2d152db7
Yasmin Araujo <yasminoa@hotmail.com>
2020-06-16 12:36:27 -0300

    Finish Atividade3

--- Atividade3/main.c
+++ Atividade3/main.c
@@ -1,39 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "adjacency_list.h"
+#include "directed_adjacency_list.h"
 #include "queue.h"
 
 int main()
 {
-    int V; // Número de tarefas para produzir item
-    int E; // Número de relações de dependência
-    int a, b;   // b depende de a
+    int V;        // Número de tarefas para produzir item
+    int E;        // Número de relações de dependência
+    int a, b;     // Vértices onde b depende de a
     int time = 0; // Menor tempo necessário para a execução de todas as tarefas
 
+    // Leitura de dados
     scanf(" %d", &V);
     scanf(" %d", &E);
 
-    AdjList *dependents = create_graph(V);
-    int* requirements = (int*) calloc(V, sizeof(int));
-    QUEUE* queue = create_queue();
+    // Estruturas de dados auxiliares
+    DirAdjList *dependents = create_graph(V);              // Lista de Adjacencias para os dependentes de cada vértice
+    int *requirements = (int *)calloc(V, sizeof(int));  // Array de inteiros para número de requisitos de cada vértice
+    QUEUE *queue = create_queue();                      // Fila auxiliar para valores a serem processados ao mesmo tempo
 
-    for(int i = 0; i < E; i ++)
+    // Leitura das relações de dependência
+    for (int i = 0; i < E; i++)
     {
         scanf(" %d", &a);
         scanf(" %d", &b);
-        // insere na lista
-        insert_edge(dependents, a, b);  // a aponta pra b (b como adj de a)
+        
+        // Atualiza lista de adj. e array de requisitos
+        insert_edge(dependents, a, b); 
         requirements[b]++;
     }
 
     // No pior caso não teremos tarefas executadas em paralelo, tendo como menor tempo o número total de vértices
-    for(int j = 0; j < V; j++)
+    for (int j = 0; j < V; j++)
     {
         // Procura vértices sem requisitos
-        for(int i = 0; i < V; i++)
+        for (int i = 0; i < V; i++)
         {
             // Se vértice não tem requisitos ele é adicionado na fila e marcado como processado
-            if(requirements[i] == 0)
+            if (requirements[i] == 0)
             {
                 requirements[i] = -1;
                 queue_push(queue, i);
@@ -41,26 +45,30 @@ int main()
         }
 
         // Caso fila esteja vazia todos os vértices possiveis já foram processados
-        if(queue_empty(queue)) break;
+        if (queue_empty(queue))
+            break;
 
         // Processa vértices encontrados
-        while(!queue_empty(queue))
+        while (!queue_empty(queue))
         {
             int v = queue_pop(queue);
-            int next = get_value(dependents, v, 0);
-            while(next != -1)
+            int i = 0;
+            int next = get_value(dependents, v, i++);
+
+            // Para cada nó adjacente decrementa o array de requisitos
+            while (next != -1)
             {
                 requirements[next]--;
-                next = get_value(dependents, v, 0);
+                next = get_value(dependents, v, i++);
             }
         }
         time++;
     }
 
-    // Checa se existe algum ciclo no grafo
-    for(int i = 0; i < V; i++)
+    // Checa se existe algum ciclo no grafo - array com algum valor diferente de -1
+    for (int i = 0; i < V; i++)
     {
-        if(requirements[i] != -1)
+        if (requirements[i] != -1)
         {
             time = -1;
             break;
@@ -70,22 +78,6 @@ int main()
     printf("%d", time);
 
     free(requirements);
+    delete_graph(dependents);
     return 0;
 }
-
-/** *
- * criar uma lista de adj -> meus dependentes (dependents)
- * criar um array tamanho V inicializado com 0 (requirements) -> ir somando qtd de requisitos que ele tem
- *      cada vez que processar um vertice decrementa 1 de todos os adjacentes
- * criar uma fila para colocar os nós a serem processados na rodada
- * em cada rodada processar todos os que tem valor 0 no array
- *
- * procura os 0 no array
- * insere na fila e deixa como -1
- * enquanto a fila nao esta vazia
- *      tira o primeiro valor 
- *      processa -> decrementa todos os adj
- * time++
- * 
- * checa array inteiro -> se algum != -1 -> retorna -1 (existe um ciclo)
- */
